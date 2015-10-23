feaf3f564540069cdcf3c54d2a544efe65781be4
Rohit Arora <rohit.rohitseju@gmail.com>
2015-10-23 16:15:14 +0530

    now you can run console using pinterest only

--- app/Console/Commands/Pinterest.php
+++ app/Console/Commands/Pinterest.php
@@ -29,7 +29,7 @@ class Pinterest extends Command
      *
      * @var string
      */
-    protected $signature = 'pinterest:like';
+    protected $signature = 'pinterest';
 
     protected $userAgents = ['Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
                              'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2217.88 Safari/537.36',
